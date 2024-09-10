68628b92f878ff4030515477b133b553618f7eec
mohadeseshafiei <mohadeseshafieeii@gmail.com>
2024-09-10 10:01:35 +0330

    add reply view

--- social/A/home/forms.py
+++ social/A/home/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
 
 
 
@@ -7,3 +7,20 @@ class PostCreateUpdateForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('body',)
+
+
+
+class CommentCreateForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body',)
+        widgets = {
+            'body': forms.Textarea(attrs={'class':'form-control'})
+        }
+
+
+
+class CommentReplyForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body',)  
--- social/A/home/views.py
+++ social/A/home/views.py
@@ -1,10 +1,14 @@
+from typing import Any
+from django.http.request import HttpRequest as HttpRequest
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View 
-from .models import Post
+from .models import Post, Comment
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
-from .forms import PostCreateUpdateForm
+from .forms import PostCreateUpdateForm, CommentCreateForm, CommentReplyForm
 from django.utils.text import slugify
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
 
@@ -16,11 +20,27 @@ class HomeView(View):
 
 
 class PostDatailView(View):
-    def get(self, request, post_id, post_slug):
-        post = get_object_or_404(Post, pk=post_id, slug=post_slug)
-        #post = Post.objects.get(pk=post_id, slug=post_slug)
-        comments = post.pcomments.filter(is_reply=False)
-        return render(request, 'home/detail.html', {'post':post, 'comments':comments})
+    form_class = CommentCreateForm
+    form_class_reply = CommentReplyForm
+
+    def setup(self, request, *args, **kwargs):
+        self.post_instance = get_object_or_404(Post, pk=kwargs['post_id'], slug=kwargs['post_slug'])
+        return super().setup(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):  
+        comments = self.post_instance.pcomments.filter(is_reply=False)
+        return render(request, 'home/detail.html', {'post':self.post_instance, 'comments':comments, 'form':self.form_class, 'reply_form':self.form_class_reply})
+    
+    @method_decorator(login_required)
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.user = request.user
+            new_comment.post = self.post_instance
+            new_comment.save()
+            messages.success(request, 'Your comment submitted successfully', 'success')
+            return redirect('home:post_detail', self.post_instance.id, self.post_instance.slug)
     
 
 
@@ -95,3 +115,24 @@ class PostCreateView(LoginRequiredMixin, View):
             new_post.save()
             messages.success(request, 'you create a new post', 'success')
             return redirect('home:post_detail', new_post.id, new_post.slug)           
+        
+
+
+
+class PostAddReplyView(LoginRequiredMixin, View):
+    form_class = CommentReplyForm
+
+
+    def post(self, request, post_id, comment_id):
+        post = get_object_or_404(Post, id=post_id)
+        comment = get_object_or_404(Comment, id=comment_id)
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            reply = form.save(commit=False)
+            reply.user = request.user
+            reply.post = post
+            reply.reply = comment
+            reply.is_reply = True
+            reply.save()
+            messages.success(request, 'Your reply submitted successfully', 'success')
+        return redirect('home:post_detail', post.id, post.slug)
